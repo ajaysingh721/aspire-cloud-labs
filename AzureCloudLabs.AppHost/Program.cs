6e82530be7b7d2e88579e0f256d6272d84b3bd0d
Ajay Singh <ajay.singh721@gmail.com>
2025-01-14 20:46:05 +0530

    Switch from Cosmos DB to SQL Server
    Updated project files to replace Cosmos DB packages with SQL Server packages. Modified `TemperatureF` calculation in `WeatherForecast.cs` to include a null-forgiving operator. Reformatted method chaining in `Program.cs` for adding SQL Server database.

--- AzureCloudLabs.AppHost/Program.cs
+++ AzureCloudLabs.AppHost/Program.cs
@@ -5,7 +5,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache");
 
 var sqlServer = builder.AddSqlServer("sql-db")
-    .AddDatabase("WeatherDB");
+    .AddDatabase("WeatherDB");  
 
 var api = builder.AddProject<Projects.AzureCloudLabs_API>("azurecloudlabs-api")
     .WithReference(cache)
