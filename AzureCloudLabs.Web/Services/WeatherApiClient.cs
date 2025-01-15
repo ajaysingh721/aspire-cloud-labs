1b88f0a184c90ea2826a406ea4ba537c429de5a3
Ajay Singh <ajay.singh721@gmail.com>
2025-01-15 20:28:23 +0530

    Add HttpClient support to WeatherServiceClient
    - Added `using AzureCloudLabs.Web.Models;` directive to `WeatherApiClient.cs`.
    - Modified `WeatherServiceClient` to include a constructor accepting an `HttpClient` parameter.
    - Updated `GetWeatherForecasts` method to use the `httpClient` instance for fetching data.
    - Improved `GetWeatherForecasts` to return an empty list if the result is null.

--- AzureCloudLabs.Web/Services/WeatherApiClient.cs
+++ AzureCloudLabs.Web/Services/WeatherApiClient.cs
@@ -5,17 +5,10 @@ namespace AzureCloudLabs.Web.Services
 {
     public class WeatherServiceClient(HttpClient httpClient)
     {
-
-
         public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts()
         {
             var result = await httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>("weatherforecast");
-
             return result ?? new List<WeatherForecast>();
         }
-
-
-        
-
     }
 }
